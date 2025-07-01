71a97940d1488b55a891fb3cfc9a74280ad8ef7a
joshuatuan <itsjoshuatuan+git@gmail.com>
2025-07-01 17:43:08 +0800

    Final submission, FECP-Java-Session1-AreaCalculator

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Joshua Miguel Tuan
+## Lab 4: Area Calculator
+### Output:
+![Screenshot 1](screenshot1.png)
+
+![Screenshot 2](screenshot2.png)
+
+![Screenshot 3](screenshot3.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,67 @@
 package org.example;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Area Calculator ===");
+        System.out.println("1. Circle");
+        System.out.println("2. Triangle");
+        System.out.println("3. Rectangle");
 
+        System.out.print("Choose a shape (1-3): ");
+        int option = sc.nextInt();
+
+        switch (option) {
+            case 1: // circle
+                System.out.print("Enter the radius: ");
+                double radius = sc.nextDouble();
+
+                double areaCircle = calculateCircleArea(radius);
+                System.out.printf("Area of the circle: %.2f\n",areaCircle );
+                break;
+
+            case 2: // triangle
+                System.out.print("Enter the base: ");
+                int base = sc.nextInt();
+                System.out.print("Enter the height: ");
+                int height = sc.nextInt();
+
+                double areaTriangle = calculateTriangleArea(base, height);
+                System.out.printf("Area of the triangle: %.2f\n", areaTriangle);
+                break;
+
+            case 3: // rectangle
+                System.out.print("Enter the length: ");
+                int length = sc.nextInt();
+                System.out.print("Enter the width: ");
+                int width = sc.nextInt();
+
+                double areaRectangle = calculateRectangleArea(length, width);
+                System.out.printf("Area of the rectangle: %.2f\n", areaRectangle);
+                break;
+
+            default:
+                System.out.println("Invalid option.");
+        }
+
+        sc.close();
+    }
+
+
+    public static double calculateCircleArea(double radius) {
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+
+    public static double calculateTriangleArea(double base, double height) {
+        double area = 0.5 * base * height;
+        return area;
+    }
+
+    public static double calculateRectangleArea(double length, double width) {
+        double area = length * width;
+        return area;
     }
 }
